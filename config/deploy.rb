50dbfb2704f3612465f0bb3b82c7a63c52d09ab4
Jaroslav Rytíř <jaroslav.rytir@ff.cuni.cz>
2010-06-24 18:49:03 +0200

    renamed:    config/vlad-deploy.rb -> config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,6 @@
+
 set :application, "sirena"
 set :user, "rytijaff"
-set :use_sudo, false
-ssh_options[:forward_agent] = true
-default_run_options[:pty] = true
-
-set :repository, "git@github.com:rytir/sirena.git"
+set :domain, "#{user}@praha5.ff.cuni.cz"
 set :deploy_to, "/opt/ruby/railapps/urls/"
-
-set :scm, :git
-set :git_enable_submodules, 1
-
-set :port, 22
-set :location, "praha5.ff.cuni.cz"
-
-
-role :web, location
-role :app, location
-role :db,  location, :primary => true
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-namespace :deploy do
-   task :restart, :roles => :app do
-     run "touch #{current_path}/tmp/restart.txt'"
-   end
-end
+set :repository, "git@github.com:rytir/sirena.git"
--- config/vlad-deploy.rb
+++
@@ -1,6 +0,0 @@
-
-set :application, "sirena"
-set :user, "rytijaff"
-set :domain, "#{user}@praha5.ff.cuni.cz"
-set :deploy_to, "/opt/ruby/railapps/urls/"
-set :repository, "git@github.com:rytir/sirena.git"
